d8e74cbf93616023fffd4302520e3a5ee5fb54fe
Kazuya Kojima <kkojima@improver.me>
2019-12-15 09:32:11 +0900

    Vuetify v2の導入

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,60 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'home' }">ホーム</router-link> |
-      <router-link :to="{ name: 'about' }">このサイトについて</router-link>
-    </div>
-    <main>
-      <router-view/>
-    </main>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
+<script>
+import HelloWorld from './components/HelloWorld';
 
-  > main {
-    display: flex;
-    justify-content: center;
-  }
-}
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
+  components: {
+    HelloWorld,
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
