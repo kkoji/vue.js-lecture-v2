281c0e5070c3d2c3bcbdec076f85cb525a43ecad
Kazuya Kojima <kkojima@improver.me>
2019-12-15 20:37:58 +0900

    Firebaseのセットアップ

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
