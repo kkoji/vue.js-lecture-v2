fcce1e66f69e6ecf2c24a30b1f40c4b1a8be8e3e
Kazuya Kojima <kkojima@improver.me>
2019-12-15 13:55:59 +0900

    連絡先編集フォームの追加

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Addresses from '../views/Addresses.vue'
+import AddressForm from '../views/AddressForm.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/addresses',
     name: 'addresses',
     component: Addresses
+  },
+  {
+    path: '/addresses/:address_id?/edit',
+    name: 'address_edit',
+    component: AddressForm
   },
   {
     path: '/about',
