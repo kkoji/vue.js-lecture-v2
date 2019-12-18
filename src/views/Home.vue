984e9108217314352e421e56d46c32be48e79d93
Kazuya Kojima <kkojima@improver.me>
2019-12-18 09:25:01 +0900

    ログイン機能を実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="home">
-  </div>
+  <v-container text-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>マイアドレス帳</h1>
+        <p>マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。</p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color='info' @click="login">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { mapActions } from 'vuex'
 
 export default {
-  name: 'home'
+  methods: {
+    ...mapActions(['login'])
+  }
 }
 </script>
